2145b306570c4c2299e2aa384a836fc2ef03e050
Travis Ormsby <t.ormsby@esri.com>
2024-11-07 12:59:52 -0600

    Trust only a single remote machine at a time

--- multicopy.ps1
+++ multicopy.ps1
@@ -10,19 +10,19 @@ param(
  )
 
 $machinesArray = Get-Content($IPsFile)
-$machinesString = $machinesArray -join ", "
 $credential = Get-Credential
 
-# Machines must be trusted to created PSSession by IP address
-$currentTrustedHosts = (get-item wsman:\localhost\Client\TrustedHosts).value
-if ($currentTrustedHosts -ne "") {
-    Set-Item wsman:\localhost\Client\TrustedHosts -value "$currentTrustedHosts, $machinesString" -Force
-} else {
-    Set-Item wsman:\localhost\Client\TrustedHosts -value $machinesString -Force
-}
-
 # Copy the file to each machine
+# Machines must be trusted to created PSSession by IP address
+
+$currentTrustedHosts = (get-item wsman:\localhost\Client\TrustedHosts).value
+
 foreach ($machine in $machinesArray) {
+    if ($currentTrustedHosts -ne "") {
+        Set-Item wsman:\localhost\Client\TrustedHosts -value "$currentTrustedHosts, $($machine.ToString())" -Force
+    } else {
+        Set-Item wsman:\localhost\Client\TrustedHosts -value $machine.ToString() -Force
+    }
     $session = New-PSSession -ComputerName $machine -Credential $credential
     Copy-Item -Path $Source -Destination $Destination -ToSession $session
 }
